bc5ec6d08145e4b801f20a04ce669ee3ae159251
nelg62 <glen.harding.nz@gmail.com>
2024-02-20 13:23:21 +1300

    third commit

---
+++ Webpagecolumntest/webpagetest.js
@@ -0,0 +1,18 @@
+let li = `
+<li class="listitem">
+                    <div class="listdiv"><input type="checkbox"></div>
+                    <div class="listdiv"></div>
+                    <div class="listdiv">12345</div>
+                    <div class="listdiv">Test</div>
+                    <div class="listdiv" id="statusbox">Test</div>
+                    <div class="listdiv"><img src="user.png" alt=""></div>
+                    
+                </li>`;
+
+function addbtnfun() {
+document.getElementById("unlisteditem").innerHTML += li
+}
+
+function removebtnfun() {
+    
+}
