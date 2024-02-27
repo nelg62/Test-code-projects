bf6aa65d8ac766140b5d81da9a3973b7b533be31
nelg62 <glen.harding.nz@gmail.com>
2024-02-27 17:10:32 +1300

    add secound image to object

--- object test/objecttest.js
+++ object test/objecttest.js
@@ -3,9 +3,9 @@ const userObj = {
     lastName: "Harding",
     randomNumber: Math.floor(Math.random() * 10) + 1,
     item: ["apple","laptop","book"],
-    userimg: "user.png",
+    userimg: ["user.png","ice-cream.png"],
 }
 
 document.getElementById('userlisttop').innerHTML = `<li>${userObj.firstName}</li>
 <li>${userObj.lastName}</li> <li>${userObj.randomNumber}</li> <li>${userObj.item}</li>
-<img src="${userObj.userimg}" style="height: 100px; width: 100px;">`
+<img src="${userObj.userimg[0]}" style="height: 100px; width: 100px;">`
