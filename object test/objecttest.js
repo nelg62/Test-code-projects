416161d97f58e54ba3bf6be41aa3447fa348c95b
nelg62 <glen.harding.nz@gmail.com>
2024-02-27 16:04:33 +1300

    testing Objects in javascript

---
+++ object test/objecttest.js
@@ -0,0 +1,11 @@
+const userObj = {
+    firstName: "Glen",
+    lastName: "Harding",
+    randomNumber: Math.floor(Math.random() * 10) + 1,
+    item: ["apple","laptop","book"],
+    userimg: "user.png",
+}
+
+document.getElementById('userlisttop').innerHTML = `<li>${userObj.firstName}</li>
+<li>${userObj.lastName}</li> <li>${userObj.randomNumber}</li> <li>${userObj.item}</li>
+<img src="${userObj.userimg}">`
