fd1b66a850a279b8b881c16190b371c2a8a8e41e
nelg62 <glen.harding.nz@gmail.com>
2024-02-28 15:19:25 +1300

    updates as typing in input box

---
+++ Userprofile/userprofile.js
@@ -0,0 +1,7 @@
+function firstnameinp() {
+   document.getElementById('pfirstname').innerHTML = `Welcome: ${document.getElementById('inpfirstname').value}`
+}
+
+function lastnameinp() {
+    document.getElementById('plastname').innerHTML = document.getElementById('inplastname').value
+ }
