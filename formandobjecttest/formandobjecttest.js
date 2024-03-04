6dda002df8839b7dd27c719ff0c38af3a255cfaf
nelg62 <glen.harding.nz@gmail.com>
2024-03-04 16:25:06 +1300

    add comments to files

--- formandobjecttest/formandobjecttest.js
+++ formandobjecttest/formandobjecttest.js
@@ -1,19 +1,25 @@
+// find the form and link to variable form
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    
-    let foodchoice = [];
 
+// function for listening to clicking submit button in form 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();                              //prevent default action of button refreshing page 
+    const formData = new FormData(form);
+
+    let foodchoice = [];                              //create object for checkboxes
+
+    // find checkboxes by name and add them to the object foodchoice
     document.querySelectorAll('[name="foodoption"]').forEach(item => {
         if (item.checked === true) {
             foodchoice.push(item.value);
         }
     })
-    
+
+    // stringafy foodcoice object and append to formData object with key FoodChoices
     formData.append('FoodChoices', JSON.stringify(foodchoice))
-    
+
+    // make from array of arrays to object
     const obj = Object.fromEntries(formData);
 
     console.log(obj)
@@ -23,8 +29,8 @@ form.addEventListener('submit', (e) => {
     // get amout of options chosen from checkbox 
     foodobjleng = JSON.parse(obj.FoodChoices).length
 
-   
 
+    // create HTML elements withobjects data
     document.getElementById('userdata').innerHTML = `
     <p>your name is ${obj.fname} ${obj.lname}</p>
     <p>your password is ${obj.pwd}</p>
@@ -34,6 +40,7 @@ form.addEventListener('submit', (e) => {
     <ul id="foodlist">
     </ul>`
 
+    // create list items in HTML based on what is selected 
     for (let i = 0; i < foodobjleng; i++) {
         document.getElementById('foodlist').innerHTML += `
         <li>${JSON.parse(obj.FoodChoices)[i]}</li>`
