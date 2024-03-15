4e2540ddfaf639df5ac04adc9da9ac20baccb08b
nelg62 <glen.harding.nz@gmail.com>
2024-03-15 14:11:15 +1300

    reduce test

---
+++ Reducemethodtest/redusetest.js
@@ -0,0 +1,15 @@
+const items = [
+    { name: 'Apple', price: 1 },
+    { name: 'Orange', price: 2 },
+    { name: 'Mango', price: 3 },
+  ];
+
+
+
+
+
+
+
+
+
+// ref https://www.freecodecamp.org/news/how-to-use-javascript-array-reduce-method/
