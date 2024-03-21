f2e0cfeb1458f61e599e76db4c65dcca08d85c05
nelg62 <glen.harding.nz@gmail.com>
2024-03-22 12:39:41 +1300

    can add and remove from form will add to this

--- todolisandshow/todolistshow.js
+++ todolisandshow/todolistshow.js
@@ -1,3 +1,15 @@
 const form = document.querySelector('form');
 
-console.log(form)
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    document.querySelector('#addItems').innerHTML += `<li class="listitem">First Name: ${formData.get("firstName")} Last Name: ${formData.get("lastName")} <button onclick="removecurrentitem(this)">Remove</button></li>`
+
+})
+
+
+
+function removecurrentitem(e) {
+    e.parentNode.parentNode.removeChild(e.parentNode)
+}
