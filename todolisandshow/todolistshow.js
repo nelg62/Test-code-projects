9e1077b5fb0b4c9efc18d3aff0862e955cae9b0e
nelg62 <glen.harding.nz@gmail.com>
2024-03-22 13:26:34 +1300

    added id

--- todolisandshow/todolistshow.js
+++ todolisandshow/todolistshow.js
@@ -1,10 +1,21 @@
 const form = document.querySelector('form');
 
+let itmlgth = 1
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
 
-    document.querySelector('#addItems').innerHTML += `<li class="listitem">First Name: ${formData.get("firstName")} Last Name: ${formData.get("lastName")} <button onclick="removecurrentitem(this)">Remove</button></li>`
+    
+    
+    document.querySelector('#addItems').innerHTML += 
+    `<li class="listitem">
+    <div>${itmlgth++}</div>
+    <div>First Name: ${formData.get("firstName")}</div> 
+    <div>Last Name: ${formData.get("lastName")}</div> 
+    <button onclick="removecurrentitem(this)">Remove</button>
+    </li>`
+
 
 })
 
