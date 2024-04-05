44b980d42d61b6bc4e2623f67565d5e43265fb66
nelg62 <glen.harding.nz@gmail.com>
2024-04-05 14:44:41 +1300

    testing api and adding to html

---
+++ todolistwithapicall/todolistapicall.js
@@ -0,0 +1,66 @@
+async function fetchURLData(url) {
+    try {
+        const mapUrl = url.map((url) => fetch(url))
+        const promiseUrl = await Promise.all(mapUrl)
+        const mapPromise = promiseUrl.map((promiseUrl) => promiseUrl.text())
+        console.log("console of URL:",await Promise.all(mapPromise).then(output1 => output1.map(output2 => JSON.parse(output2)))) //makes the text into json and usable with parse
+    } catch (error){
+        console.log("error info:"+ error)
+
+    }
+}
+
+document.getElementById("apitest").innerHTML = 
+
+fetchURLData(['https://jsonplaceholder.typicode.com/todos/1','https://jsonplaceholder.typicode.com/todos/2','https://jsonplaceholder.typicode.com/todos/3'])
+
+
+// data from from into variable
+const form = document.querySelector('form');
+
+// variable for id number
+let itmlgth = 1
+
+// create a constructor for person object 
+class Person {
+    constructor(first, last, animal, food) {
+        this.first = first
+        this.last = last
+        this.animal = animal
+        this.food = food
+    }
+}
+
+// container object for person constructor 
+const newPeople = []
+
+// when data from from is submited 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+
+    // add the data from from to ul with ID addItems
+    document.querySelector('#addItems').innerHTML += 
+    `<li class="listitem">
+    <div>${itmlgth++}</div>
+    <div>First Name: ${formData.get("firstName")}</div> 
+    <div>Last Name: ${formData.get("lastName")}</div> 
+    <div>Animal: ${formData.get("animalchoice")}</div>
+    <div>Food: ${formData.getAll("foodoption")}</div>
+    <button onclick="removecurrentitem(this)">Remove</button>
+    </li>`
+
+    // push data from form into newPeople object 
+   newPeople.push(new Person(formData.get("firstName"), formData.get("lastName"), formData.get("animalchoice"), formData.getAll("foodoption")))
+
+    console.log(newPeople)
+
+})
+
+
+// function to remove current item when clicking remove button 
+function removecurrentitem(e) {
+    e.parentNode.parentNode.removeChild(e.parentNode)
+}
